0d5fea25e24404534124296bf869726282098fde
ojbgigi <ojbgigi@gmail.com>
2023-08-03 19:46:50 +0800

    Toggle theme using ThemeProvider and useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { useState } from "react";
 import { ReactComponent as AirFlowIcon } from "./images/airFlow.svg";
 import { ReactComponent as DayCloudyIcon } from "./images/day-cloudy.svg";
 import { ReactComponent as RainIcon } from "./images/rain.svg";
 import { ReactComponent as RefreshIcon } from "./images/refresh.svg";
 import styled from "@emotion/styled";
+import { ThemeProvider } from "@emotion/react";
+
+// 定義深淺色主題配色
+const theme = {
+  light: {
+    backgroundColor: "#ededed",
+    foregroundColor: "#f9f9f9",
+    boxShadow: "0 1px 3px 0 #999999",
+    titleColor: "#212121",
+    temperatureColor: "#757575",
+    textColor: "#828282",
+  },
+  dark: {
+    backgroundColor: "#1F2022",
+    foregroundColor: "#121416",
+    boxShadow:
+      "0 1px 4px 0 rgba(12, 12, 13, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.15)",
+    titleColor: "#f9f9fa",
+    temperatureColor: "#dddddd",
+    textColor: "#cccccc",
+  },
+};
 
 const Container = styled.div`
-  background-color: #ededed;
+  background-color: ${({ theme }) => theme.backgroundColor};
   height: 100%;
   display: flex;
   align-items: center;
@@ -15,21 +38,21 @@ const Container = styled.div`
 const WeatherCard = styled.div`
   position: relative;
   min-width: 360px;
-  box-shadow: 0 1px 3px 0 #999999;
-  background-color: #f9f9f9;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+  background-color: ${({ theme }) => theme.foregroundColor};
   box-sizing: border-box;
   padding: 30px 15px;
 `;
 
 const Location = styled.div`
   font-size: 28px;
-  color: #212121;
+  color: ${({ theme }) => theme.titleColor};
   margin-bottom: 20px;
 `;
 
 const Description = styled.div`
   font-size: 16px;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   margin-bottom: 30px;
 `;
 
@@ -41,7 +64,7 @@ const CurrentWeather = styled.div`
 `;
 
 const Temperature = styled.div`
-  color: #757575;
+  color: ${({ theme }) => theme.temperatureColor};
   font-size: 96px;
   font-weight: 300;
   display: flex;
@@ -57,7 +80,7 @@ const AirFlow = styled.div`
   align-items: center;
   font-size: 16x;
   font-weight: 300;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   margin-bottom: 20px;
 
   svg {
@@ -72,7 +95,7 @@ const Rain = styled.div`
   align-items: center;
   font-size: 16x;
   font-weight: 300;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
 
   svg {
     width: 25px;
@@ -85,14 +108,17 @@ const DayCloudy = styled(DayCloudyIcon)`
   flex-basis: 30%;
 `;
 
+const Footer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-top: 24px;
+  align-items: center;
+`;
+
 const Refresh = styled.div`
-  position: absolute;
-  right: 15px;
-  bottom: 15px;
   font-size: 12px;
-  display: inline-flex;
-  align-items: flex-end;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
+  display: flex;
 
   svg {
     margin-left: 10px;
@@ -102,30 +128,53 @@ const Refresh = styled.div`
   }
 `;
 
+const Theme = styled.div`
+  font-size: 12px;
+  background: ${({ theme }) => theme.backgroundColor};
+  padding: 8px 16px;
+  border-radius: 40px;
+  color: ${({ theme }) => theme.titleColor};
+  cursor: pointer;
+`;
+
 const App = () => {
+  const [currentTheme, setCurrentTheme] = useState("light");
+
+  const handleToggleTheme = () => {
+    const newTheme = currentTheme === "light" ? "dark" : "light";
+    setCurrentTheme(newTheme);
+  };
+
   return (
-    <Container>
-      <WeatherCard>
-        <Location>台北市</Location>
-        <Description>多雲時晴</Description>
-        <CurrentWeather>
-          <Temperature>
-            23 <Celsius>°C</Celsius>
-          </Temperature>
-          <DayCloudy />
-        </CurrentWeather>
-        <AirFlow>
-          <AirFlowIcon /> 23 m/h
-        </AirFlow>
-        <Rain>
-          <RainIcon />
-          48%
-        </Rain>
-        <Refresh>
-          最後觀測時間：上午 12:03 <RefreshIcon />
-        </Refresh>
-      </WeatherCard>
-    </Container>
+    <ThemeProvider theme={theme[currentTheme]}>
+      <Container>
+        <WeatherCard>
+          <Location>台北市</Location>
+          <Description>多雲時晴</Description>
+          <CurrentWeather>
+            <Temperature>
+              23 <Celsius>°C</Celsius>
+            </Temperature>
+            <DayCloudy />
+          </CurrentWeather>
+          <AirFlow>
+            <AirFlowIcon /> 23 m/h
+          </AirFlow>
+          <Rain>
+            <RainIcon />
+            48%
+          </Rain>
+          <Footer>
+            <Theme onClick={handleToggleTheme}>
+              切換主題為{currentTheme === "light" ? "深色" : "淺色"}模式
+            </Theme>
+            <Refresh>
+              最後觀測時間：上午 12:03 <RefreshIcon />
+            </Refresh>
+          </Footer>
+        </WeatherCard>
+      </Container>
+    </ThemeProvider>
   );
 };
 
